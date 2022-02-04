94f31014a5cd9fc0b26ff93fd17bd666ca0739c8
JaredJose <jjose@ucsd.edu>
2022-02-03 19:37:33 -0800

    created example and example tester

---
+++ Example.java
@@ -0,0 +1,5 @@
+class Example{
+    public static int adder(int num1, int num2){
+        return num1 + num2;
+    }
+}
---
+++ ExampleTester.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ExampleTester {
+    @Test
+    public void testAdder(){
+        int num1 = 1;
+        int num2 = 2;
+        assertEquals(3, Example.adder(num1, num2));
+    }
+}
